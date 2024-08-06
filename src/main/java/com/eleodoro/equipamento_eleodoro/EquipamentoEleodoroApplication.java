17783d26121976f8d8c6469b1af75d5fd751983f
julia-kw <knack.julia@escola.pr.gov.br>
2024-08-06 12:03:49 -0300

    equipamento-eleodoro

---
+++ src/main/java/com/eleodoro/equipamento_eleodoro/EquipamentoEleodoroApplication.java
@@ -0,0 +1,15 @@
+package com.eleodoro.equipamento_eleodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class EquipamentoEleodoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipamentoEleodoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eleodoro/equipamento_eleodoro/EquipamentoEleodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleodoro.equipamento_eleodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipamentoEleodoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
