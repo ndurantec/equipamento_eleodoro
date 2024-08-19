a95d04169e099d1c1b24734c9b902d41b71e9f31
Caaasdx <cauan.dias@escola.pr.gov.br>
2024-08-19 11:55:21 -0300

    criei @entity e @Id

--- src/main/java/com/eleodoro/equipamento_eleodoro/fornecedor.java
+++ src/main/java/com/eleodoro/equipamento_eleodoro/fornecedor.java
@@ -1,11 +1,28 @@
 package com.eleodoro.equipamento_eleodoro;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class fornecedor {
+
+    private static final Long serialVersionUID = 1L; 
+
+ @Id
+ @GeneratedValue (strategy = GenerationType.IDENTITY)
+ private Long id;
+
+
+
+
   private String nome;
   private String telefone;
   private String endereco;
 
-
+   
     public fornecedor() {
     }
 
