6ec97d3d047205405ac4aae8322a95080f8d4386
knack.julia@escola.pr.gov.br <knack.julia@escola.pr.gov.br>
2024-08-20 11:56:54 -0300

    adcionei no arquivo ManutencaoController

--- src/main/java/com/eleodoro/equipamento_eleodoro/controller/ManutencaoController.java
+++ src/main/java/com/eleodoro/equipamento_eleodoro/controller/ManutencaoController.java
@@ -1,5 +1,15 @@
 package com.eleodoro.equipamento_eleodoro.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/manutencao")
 public class ManutencaoController {
     
+    @GetMapping(value = "imprimir")
+      public void imprimir(){
+        System.out.println("chegou na classe manutencao");
+      }
 }
