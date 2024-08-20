f64431652a7dfcbdbc3aa5c71f8fa9b53f247c97
MiguelFSoda <miguel.ferreira.santos@escola.pr.gov.br>
2024-08-20 12:05:23 -0300

    CadastroController.java

---
+++ src/main/java/com/eleodoro/equipamento_eleodoro/controller/CadastroController.java
@@ -0,0 +1,28 @@
+package com.eleodoro.equipamento_eleodoro.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cadastro")
+public class CadastroController {
+ 
+
+
+    @GetMapping(value = "imprimir")
+    public void imprimir(){
+        System.out.print("chegou em cadastro");
+
+    }
+    
+
+
+
+
+
+
+
+
+
+}
