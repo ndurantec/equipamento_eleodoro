6f7c065b2bc61238aad6bc3724ea319e26d26172
Caaasdx <cauan.dias@escola.pr.gov.br>
2024-09-10 12:16:47 -0300

    update do controller(não terminado)

--- src/main/java/com/eleodoro/equipamento_eleodoro/controller/FornecedorController.java
+++ src/main/java/com/eleodoro/equipamento_eleodoro/controller/FornecedorController.java
@@ -1,8 +1,17 @@
 package com.eleodoro.equipamento_eleodoro.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.eleodoro.equipamento_eleodoro.dto.FornecedorDTO;
+import com.eleodoro.equipamento_eleodoro.model.Fornecedor;
 
 @RestController
 @RequestMapping(value = "/fornecedor")
@@ -13,5 +22,32 @@ public class FornecedorController {
 
        return "chegou ate fornecedor";
 
+    }
+    @PostMapping (value= "/insert")
+    public ResponseEntity<Fornecedor> insert (@RequestBody FornecedorDTO fornecedorDTO) {
+
+        Fornecedor novoFornecedor=fornecedorDTO.novoFornecedor();
+        fornecedorDTORepository.save(novoFornecedor);
+
+        System.out.println("chegou no método insert");
+        System.out.println(fornecedorDTO.toString());
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path{"{/id}")
+        .BuildAndExpand(Fornecedor.getID())
+        .toUri();
+
+        return ResponseEntity.created(uri).body(novoFornecedor);
+    }
+
+    }
+    @PutMapping(value ="/{id}"
+
+    public ResponseEntity<void> update (@PathVariable Long id, @RequestBody Fornecedor fornecedor) 
+
+        Optional<Fornecedor> fornecedorBanco=fornecedorBanco = fornecedorRepository.finalById(id);
+        
+        Fornecedor FornecedorModificado= fornecedorBanco.get();
+        Manutenç
     }
 }
